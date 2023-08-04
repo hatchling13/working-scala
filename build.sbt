39a56b45b801e6c745ef85d39143e77a570dbee1
CHEESE <81912261+ychxexn@users.noreply.github.com>
2023-08-04 18:37:10 +0900

    chore: 불필요한 코드 제거

--- build.sbt
+++ build.sbt
@@ -83,6 +83,14 @@ lazy val `cheese` = project
 lazy val `deepZIOExam` = project
   .settings(sharedSettings)
   .settings(
+    libraryDependencies ++= Seq(
+      "com.softwaremill.sttp.client3" %% "core" % "3.8.16",
+      "com.softwaremill.sttp.client3" %% "zio-json" % "3.8.16",
+      "dev.zio" %% "zio-json" % "0.5.0",
+      "com.lihaoyi" %% "os-lib" % "0.9.1",
+      "com.lihaoyi" %% "ujson" % "3.0.0",
+      "dev.zio" %% "zio-http" % "3.0.0-RC2",
+    )
   )
 
 lazy val `cookingInfo` = project.settings(
@@ -141,6 +149,29 @@ lazy val `sample-db` = project
     )
   )
 
+
+lazy val `bicycle_db` = project
+  .settings(sharedSettings)
+  .settings(
+    libraryDependencies ++= Seq(
+      "org.tpolecat" %% "doobie-core" % "1.0.0-RC2",
+      "io.github.gaelrenoux" %% "tranzactio" % "4.1.0",
+      "org.postgresql" % "postgresql" % "42.5.4",
+      "dev.zio" %% "zio-http" % "3.0.0-RC2"
+    )
+  )
+
+lazy val `sample-db-taste-review` = project
+  .settings(sharedSettings)
+  .settings(
+    libraryDependencies ++= Seq(
+      "org.tpolecat" %% "doobie-core" % "1.0.0-RC2",
+      "io.github.gaelrenoux" %% "tranzactio" % "4.1.0",
+      "org.postgresql" % "postgresql" % "42.5.4",
+      "dev.zio" %% "zio-http" % "3.0.0-RC2"
+    )
+  )
+
 lazy val `tabling` = project
   .settings(sharedSettings)
   .settings(
@@ -151,3 +182,4 @@ lazy val `tabling` = project
       "org.postgresql" % "postgresql" % "42.5.4",
     )
   )
+
--- deepZIOExam/src/main/scala/server/ServerExample.scala
+++ deepZIOExam/src/main/scala/server/ServerExample.scala
@@ -22,7 +22,6 @@ object ServerExample extends ZIOAppDefault {
     }
 
   case class Friend(name: String, age: Int, hobbies: List[String], location: String)
-
   object Friend {
     implicit val decoder: JsonDecoder[Friend] = DeriveJsonDecoder.gen[Friend]
     implicit val encoder: JsonEncoder[Friend] = DeriveJsonEncoder.gen[Friend]
@@ -32,75 +31,4 @@ object ServerExample extends ZIOAppDefault {
     Server
       .serve(app.withDefaultErrorResponse)
       .provideLayer(Server.defaultWithPort(13333) ++ Client.default)
-  abstract class Notification
-
-  case class Email(sourceEmail: String, title: String, body: String)
-      extends Notification
-
-  case class SMS(sourceNumber: String, message: String) extends Notification
-
-  case class VoiceRecording(contactName: String, link: String)
-      extends Notification
-
-  case class Board(title: String, txt: String, notification: List[Notification])
-
-  case class Dish(name: String, ingredients: List[String], country: Country)
-
-  object Dish {
-    implicit val decoder: JsonDecoder[Dish] = DeriveJsonDecoder.gen[Dish]
-    implicit val encoder: JsonEncoder[Dish] = DeriveJsonEncoder.gen[Dish]
-  }
-
-  sealed trait Country
-
-  object Country {
-    implicit val decoder: JsonDecoder[Country] = DeriveJsonDecoder.gen[Country]
-    implicit val encoder: JsonEncoder[Country] = DeriveJsonEncoder.gen[Country]
-  }
-
-  case class Korean() extends Country
-
-  case class Japanese() extends Country
-
-  case class Chinese() extends Country
-
-  //    _ <- zio.Console.printLine("")
-  //    b = Board(
-  //      "a",
-  //      "title",
-  //      List(
-  //        Email("a", "hello english man ", "c"),
-  //        SMS("010-111203391", "아무내용이ㅣ"),
-  //        SMS("김사장", "주말출근 가능하신분?")
-  //      )
-  //    )
-  //    _ = println(b)
-  //    _ = b.notification.foreach { noti =>
-  //      val a = noti match {
-  //        case Email(sourceEmail, title, body) =>
-  //          s"이 이메일은 영국에서 시작되었으며 제목은 $title 인데....?"
-  //        case SMS(sourceNumber, message) if sourceNumber == "김사장" => "차단"
-  //        case SMS(sourceNumber, message) =>
-  //          s"국제번호에서 온 SMS 입니다 $sourceNumber, $message"
-  //        case VoiceRecording(contactName, link) =>
-  //          s"아쉽지만 text로는 들려줄 수 없네요 $contactName 직접 전화 거세요"
-  //        case _ => "뭔지 모르겠는데요?"
-  //      }
-  //      println(a)
-  //
-  //    }
-  //  } yield ()
-
 }
-
-// 배울 것
-// case class ADT <- 데이터 모델링
-// 아키텍쳐, 의존성 주입
-// http git
-
-// 안 배울 것
-// implicit
-// macro
-// 특이한 스칼라 문법
-// 모나드
-//
