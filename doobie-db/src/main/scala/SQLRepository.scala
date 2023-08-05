f04af88e6b0b5b40578c1e6032cc2f6d720f884a
chloeseo0210 <chloeseo0210@gmail.com>
2023-08-05 14:54:40 +0900

    fix : odd sql

--- doobie-db/src/main/scala/SQLRepository.scala
+++ doobie-db/src/main/scala/SQLRepository.scala
@@ -1,20 +1,21 @@
 import Mood._
+import DBMood._
 import doobie._
 import doobie.implicits._
 
 object SQLRepository {
   def getAll() =
-    sql"""SELECT * from "DailyNotes".mood order by "createdAt"""".stripMargin.query[Mood].to[List]
+    sql"""SELECT * from "DailyNotes".mood order by "createdAt"""".stripMargin.query[DBMood].to[List]
 
   def deleteOne(id: Int) = 
     sql"""DELETE FROM "DailyNotes".mood WHERE id = ($id)""".update.run
   
   def updateOne(id: Int, score:Int) =
-    sql"""UPDATE "DailyNotes".mood SET score = (${score}) WHERE id = ($id)"""
+    sql"""UPDATE "DailyNotes".mood SET score = (${score}) WHERE id = ($id)""".update.run
   
   
   def insertMood(name: String, score: Int) =
-  sql"""insert into "DailyNotes".mood (name, score) values ($name, $score)""".update.run
+    sql"""insert into "DailyNotes".mood (name, score) values ($name, $score)""".update.run
 
 
   def deleteAllMood() =
