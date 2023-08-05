23d42635c104b8cb61e20ce8766b70ebe05e6025
chloeseo0210 <chloeseo0210@gmail.com>
2023-08-05 13:44:19 +0900

    feat : extract db connection

---
+++ doobie-db/src/main/scala/PostgresConnection.scala
@@ -0,0 +1,25 @@
+import io.github.gaelrenoux.tranzactio.ConnectionSource
+import io.github.gaelrenoux.tranzactio.doobie.{Database, tzio}
+import zio._
+
+object PostgresConnection {
+  val postgres = locally {
+    val path = "localhost:5433"
+    val name = "postgres"
+    val user = "postgres"
+    val password = "1q2w3e4r"
+    s"jdbc:postgresql://$path/$name?user=$user&password=$password"
+  }
+
+  val driver = "org.postgresql.Driver"
+
+  Class.forName(driver)
+
+  val conn = ZLayer(
+    ZIO.attempt(
+      java.sql.DriverManager.getConnection(
+        postgres
+      )
+    )
+  )
+}
