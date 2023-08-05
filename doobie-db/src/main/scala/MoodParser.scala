ad5f5844b7b24fdbf2d80081c0f7d0ad738885c3
chloeseo0210 <chloeseo0210@gmail.com>
2023-08-05 14:55:22 +0900

    refactor : modify case classes to abstract due to DBError

--- doobie-db/src/main/scala/MoodParser.scala
+++ doobie-db/src/main/scala/MoodParser.scala
@@ -1,12 +1,21 @@
-case class Mood(name: String, score: Int)
+import doobie.enumerated.JdbcType
+import java.util.Date
+abstract class AbstractMood{
+  def name:String
+  def score:Int
+}
+case class Mood(name: String, score: Int) extends AbstractMood
+case class DBMood(name: String, score: Int, createdAt: Date, id: Int) extends AbstractMood
 
 object MoodParser {
-    def parseInput(score: String) =
+    def parseInsertInput(score: String) =
         score match {
         case "10" => Mood("GOOD", 10)
         case "5" => Mood("SOSO", 5)
         case "0" => Mood("BAD", 0)
         case _ => Mood("NONE", -1)
   }
-
+  
+  def parseUpdateInput(id: String): Int =
+    return id.toInt
 }
