d3000eca3fd18237f35723e47d18940784c559b3
chloeseo0210 <chloeseo0210@gmail.com>
2023-08-08 21:48:30 +0900

    format : execute after scalafmt

--- doobie-db/src/main/scala/DiaryService.scala
+++ doobie-db/src/main/scala/DiaryService.scala
@@ -4,30 +4,27 @@ import doobie.implicits._
 import doobie._
 import zio._
 import zio.{Console}
-import SQLRepository._
+import DiaryRepository._
 import MoodParser._
 
 object DiaryService {
-
-    def getAllMoods = for {
+  def getAllMoods = for {
     database <- ZIO.service[Database]
     rows <- database
       .transactionOrWiden(for {
         res <- tzio {
-            getAll
+          getAll
         }
       } yield res)
-        _ <- Console.printLine(
-        """
+    _ <- Console.printLine("""
         지금까지 기록한 Mood 목록입니다.
         _________________________________              
         """)
-        _ <- zio.Console.printLine(rows)
-    } yield ()
+    _ <- zio.Console.printLine(rows)
+  } yield ()
 
   def addTodayMood = for {
-    _ <- Console.printLine(
-    """
+    _ <- Console.printLine("""
     오늘의 기분을 점수로 입력해주세요
     _________________________________
     0점 : BAD
@@ -36,49 +33,45 @@ object DiaryService {
     """)
 
     inputScore <- Console.readLine("점수")
-    mood = parseInsertInput(inputScore)
+    targetMood <- parseInsertInput(inputScore)
 
-    database <- ZIO.service[Database]
-    rows <- database
-      .transactionOrWiden(
-      for {
-        res <- tzio {
-          deleteAllMood()
-          insertMood(mood.name, mood.score)
-          getAll()
-        }
-      } yield res)
-
-    _ <- zio.Console.printLine(rows)
-
-  } yield ()
-
-def modifyMood = for {
     database <- ZIO.service[Database]
     rows <- database
       .transactionOrWiden(for {
         res <- tzio {
-            getAll
+          insertMood(targetMood.name, targetMood.score)
         }
       } yield res)
-        _ <- Console.printLine(
-        """
+
+    _ <- zio.Console.printLine("입력 완료되었습니다.")
+    _ <- zio.Console.printLine(rows)
+
+  } yield ()
+
+  def modifyMood = for {
+    database <- ZIO.service[Database]
+    rows <- database
+      .transactionOrWiden(for {
+        res <- tzio {
+          getAll
+        }
+      } yield res)
+    _ <- Console.printLine("""
         지금까지 기록한 Mood 목록입니다.
         _________________________________              
         """)
-        _ <- zio.Console.printLine(rows)
-        inputNumber <- Console.readLine("수정할 항목을 번호로 입력해주세요 : ")
-        inputScore <- Console.readLine("수정할 점수를 입력해주세요 : ")
-        targetNumber = parseUpdateInput(inputNumber)
-        targetScore = parseUpdateInput(inputScore)
-        updatedRow <- database.transactionOrWiden(for {
-            res <-tzio {
-               updateOne(targetNumber,targetScore)
-            }
-        }
-        yield res)
+    _ <- zio.Console.printLine(rows)
+    inputNumber <- Console.readLine("수정할 항목을 번호로 입력해주세요 : ")
+    inputScore <- Console.readLine("수정할 점수를 입력해주세요 : ")
+    targetNumber = parseUpdateInput(inputNumber)
+    targetMood <- parseInsertInput(inputScore)
+    updatedRow <- database.transactionOrWiden(for {
+      res <- tzio {
+        updateOne(targetNumber, targetMood)
+      }
+    } yield res)
 
-        _ <- zio.Console.printLine(updatedRow)
+    _ <- zio.Console.printLine("수정이 완료되었습니다.")
 
   } yield ()
   def deleteMood = for {
@@ -86,24 +79,22 @@ def modifyMood = for {
     rows <- database
       .transactionOrWiden(for {
         res <- tzio {
-            getAll
+          getAll
         }
       } yield res)
-        _ <- Console.printLine(
-        """
+    _ <- Console.printLine("""
         지금까지 기록한 Mood 목록입니다.
         _________________________________              
         """)
-        _ <- zio.Console.printLine(rows)
-        inputNumber <- Console.readLine("삭제할 항목을 번호로 입력해주세요 : ")
-        targetNumber = parseUpdateInput(inputNumber)
-        updatedRow <- database.transactionOrWiden(for {
-            res <-tzio {
-               deleteOne(targetNumber)
-            }
-        }
-        yield res)
-        _ <- zio.Console.printLine(s"${targetNumber}번은 삭제되었습니다.")        
+    _ <- zio.Console.printLine(rows)
+    inputNumber <- Console.readLine("삭제할 항목을 번호로 입력해주세요 : ")
+    targetNumber = parseUpdateInput(inputNumber)
+    updatedRow <- database.transactionOrWiden(for {
+      res <- tzio {
+        deleteOne(targetNumber)
+      }
+    } yield res)
+    _ <- zio.Console.printLine(s"${targetNumber}번은 삭제되었습니다.")
   } yield ()
 
 }
