3d04bdefe504abc718c25d0e00a23ebcb0d24760
chloeseo0210 <chloeseo0210@gmail.com>
2023-09-02 09:17:51 +0900

    refactor : delete unused files

--- tabling/src/main/scala/Reservation.scala
+++
@@ -1,6 +0,0 @@
-case class Reservation(name: String,
-                       phone: String,
-                       restaurant_id: Int,
-                       reservation_date: String,
-                       reservation_time: String,
-                       guests: Int)
--- tabling/src/main/scala/Restaurant.scala
+++
@@ -1,5 +0,0 @@
-case class Restaurant(id: Int,
-                      name: String) {
-  override def toString() = s"($id) $name"
-
-}
