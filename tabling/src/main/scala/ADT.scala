d367aee4fc94da7fa7c4ad6619349abdc08bfe2d
chloeseo0210 <chloeseo0210@gmail.com>
2023-09-02 09:16:26 +0900

    refactor : extract ADTs

---
+++ tabling/src/main/scala/ADT.scala
@@ -0,0 +1,14 @@
+case class Coupon(owner: String, discount_rate: Int) {}
+
+case class Restaurant(id: Int, name: String) {
+  override def toString() = s"($id) $name"
+}
+
+case class Reservation(
+    name: String,
+    phone: String,
+    restaurant_id: Int,
+    reservation_date: String,
+    reservation_time: String,
+    guests: Int
+)
