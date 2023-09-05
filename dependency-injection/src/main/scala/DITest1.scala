16ce9855f76384f2ceaddab029e5edc60dd7cd2b
Jihun <97544861+jihun315016@users.noreply.github.com>
2023-09-05 14:48:37 +0900

    Update DITest1.scala

--- dependency-injection/src/main/scala/DITest1.scala
+++ dependency-injection/src/main/scala/DITest1.scala
@@ -4,21 +4,15 @@ import zio.{ZIO, ZIOAppDefault}
 object DITest1 extends ZIOAppDefault {
   trait 식당저장소 {
     def 모든식당이름가져오기(): ZIO[Any, Nothing, List[String]]
-
-    def 식당메뉴가져오기(name: String): ZIO[Any, Nothing, List[String]]
   }
 
 
   class 공덕식당저장소 extends 식당저장소 {
     def 모든식당이름가져오기() = ZIO.succeed(List("김밥집", "피자집", "샌드위치집"))
-
-    def 식당메뉴가져오기(name: String) = ZIO.succeed(List("참치김밥", "계란김밥", "매운김밥"))
   }
 
   class 판교식당저장소 extends 식당저장소 {
     def 모든식당이름가져오기() = ZIO.succeed(List("카카오국밥", "네이버국밥", "엔씨국밥"))
-
-    def 식당메뉴가져오기(name: String) = ZIO.succeed(List("라이언국밥", "춘식국밥"))
   }
 
 
