660c84cfe31672eea919b40c8dab3b8f29594556
chloeseo0210 <chloeseo0210@gmail.com>
2023-09-05 22:01:47 +0900

    refactor: change Restaurant

--- declarative-prog/src/main/scala/ADT.scala
+++ declarative-prog/src/main/scala/ADT.scala
@@ -1,8 +1,7 @@
+
 case class Coupon(owner: String, discount_rate: Int) {}
 
-case class Restaurant(id: Int, name: String) {
-  override def toString() = s"($id) $name"
-}
+case class Restaurant(id: Int, name: String)
 
 case class Reservation(
     name: String,
