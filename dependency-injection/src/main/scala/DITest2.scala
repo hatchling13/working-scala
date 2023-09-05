c4137e6596cafe0576aab97852e7fe48073fe6e8
Jihun <97544861+jihun315016@users.noreply.github.com>
2023-09-05 14:49:08 +0900

    Update DITest2.scala

--- dependency-injection/src/main/scala/DITest2.scala
+++ dependency-injection/src/main/scala/DITest2.scala
@@ -12,14 +12,10 @@ object DITest2 extends ZIOAppDefault {
 
   class 공덕식당저장소 extends 식당저장소 {
     def 모든식당이름가져오기() = ZIO.succeed(List("김밥집", "피자집", "샌드위치집"))
-
-    def 식당메뉴가져오기(name: String) = ZIO.succeed(List("참치김밥", "계란김밥", "매운김밥"))
   }
 
   class 판교식당저장소 extends 식당저장소 {
     def 모든식당이름가져오기() = ZIO.succeed(List("카카오국밥", "네이버국밥", "엔씨국밥"))
-
-    def 식당메뉴가져오기(name: String) = ZIO.succeed(List("라이언국밥", "춘식국밥"))
   }
 
   object 판교식당저장소 {
